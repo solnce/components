3cad8c36e56572b36708e8ea025535821dd88157
solnce <solnce.jpn@gmail.com>
2022-07-12 16:01:13 +0900

    refactoring

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 120, //1行の文字列を120文字にする
+  singleQuote: true, //ダブルに代わりシングルクオーテーションを使う
+  semi: true, //行末にセミコロンを追加
+  trailingComma: 'es5', //複数行の場合は可能な限り末尾のカンマを表示
+  tabWidth: 2, //タグのスペース２ デフォルトは2
+  useTabs: false, //スペースをタブに代える デフォルトはfalse
+}
+
