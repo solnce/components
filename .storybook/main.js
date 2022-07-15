e42e313caa037ebb983fed3cbf03ac51e4779085
solnce <solnce.jpn@gmail.com>
2022-07-16 01:53:37 +0900

    fix config file for moving directory

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,30 +1,17 @@
-const path = require('path')
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
-  stories: [
-    "../components/**/*.stories.mdx",
-    "../components/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+  stories: ['../src/components/**/*.stories.mdx', '../src/components/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   webpackFinal(config) {
-    config.resolve.modules = [
-      ...(config.resolve.modules || []),
-      path.resolve(__dirname, '../components')
-    ]
-    config.resolve.plugins = [
-      ...(config.resolve.plugins || []),
-      new TsconfigPathsPlugin()
-    ]
+    config.resolve.modules = [...(config.resolve.modules || []), path.resolve(__dirname, '../src/components')];
+    config.resolve.plugins = [...(config.resolve.plugins || []), new TsconfigPathsPlugin()];
     return config;
   },
-  framework: "@storybook/react",
+  framework: '@storybook/react',
   babel: async (options) => ({
     ...options,
-    presets: [...options.presets, "@emotion/babel-preset-css-prop"],
+    presets: [...options.presets, '@emotion/babel-preset-css-prop'],
   }),
 };
